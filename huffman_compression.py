8b64f20710bb89f5ecd2c096a83d5602ae4341d6
Ghoulboy78 <ghoulboy27@gmail.com>
2021-01-25 14:40:42 +0100

    making huffman compression work and return key

--- huffman_compression.py
+++ huffman_compression.py
@@ -1,18 +1,49 @@
 message_file=open('message.txt','r+')
 
-message=message_file.readlines()
+encryption_base=10#gonna implement as function argument later
 
-huffman_tree={}
+def __sort_chars(message):
 
-char_count={}
+    huffman_tree={}
 
-for line in message:
-    for char in [c for c in line]:
-        char_count.setdefault(char,0)
-        char_count[char]+=1
-        
-print(char_count)
+    char_count={}
 
-for char in char_count:
+    for line in message:
+        for char in [c for c in line]:
+            char_count.setdefault(repr(char),0)
+            char_count[repr(char)]+=1
+
+    return char_count
+
+def __sort_char_map(char_map):
+    return [sorted(char_map.keys(),key=char_map.get,reverse=True),char_map]
+
+
+def __get_huffman_tree(sorted_chars):
     
-    pass
+    reverse_sorted_chars=list(reversed(sorted_chars[0]))
+
+    if len(reverse_sorted_chars)<10:#if less than 10, just return the string, no need to encrypt tbh
+        exit(print(reverse_sorted_chars))
+
+    while len(reverse_sorted_chars)>1:
+        popped_chars=[]
+        for i in range(min(10,len(reverse_sorted_chars))):
+            char=reverse_sorted_chars.pop(0)
+            popped_chars.append([eval(char),sorted_chars[1].pop(char)])
+
+        pair=[[],0]
+        for c in popped_chars:
+            pair[0].append(c[0])
+            pair[1]+=c[1]
+        sorted_chars[1].setdefault(repr(pair[0]),pair[1])
+        sorted_chars=__sort_char_map(sorted_chars[1])
+        reverse_sorted_chars=list(reversed(sorted_chars[0]))
+
+
+    return eval(reverse_sorted_chars[0])
+
+def huffman_tree(message):
+    __sort_char_map(__sort_chars(message))
+
+print(str(huffman_tree(message_file.readlines())))
