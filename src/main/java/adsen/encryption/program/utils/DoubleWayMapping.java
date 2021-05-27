c85a9cdfd6558960d96ff4fa5eb452e89cb2fc8f
Advait Sen <advaitsen78@gmail.com>
2021-05-27 09:31:10 +0200

    Primary work on DoubleWayMapping.java
    Gonna fiddle with it later, after I set up the python version

--- src/main/java/adsen/encryption/program/utils/DoubleWayMapping.java
+++ src/main/java/adsen/encryption/program/utils/DoubleWayMapping.java
@@ -1,1 +1,18 @@
-//Todo fill in
+package adsen.encryption.program.utils;
+
+
+import java.util.HashMap;
+
+/**
+ * This file is purely academic, because I felt that it was a cool idea to have it and that it was an interesting challenge.
+ * It basically maps one set to another, with no duplicate keys on any side, allowing for quick 2-way lookups.
+ */
+public interface DoubleWayMapping<A, B> {
+    /**
+     * The number of value-value mappings in this Mapping
+     * @return the number of value-value entries in this map
+     */
+    int size();
+
+
+}
