794d3b682c6d643a841f120476b5f3ef2a547933
Advait Sen <advaitsen78@gmail.com>
2021-07-21 22:06:45 +0200

    Add Random object to Encoder.java
    So I don't have to declare it all over the place

--- src/main/java/adsen/encryption/program/encoders/Encoder.java
+++ src/main/java/adsen/encryption/program/encoders/Encoder.java
@@ -1,16 +1,25 @@
 package adsen.encryption.program.encoders;
 
+import java.util.Random;
+
 /**
  * The inheritor class from which all encoders must derive
  */
 public interface Encoder {
+
+    /**
+     * A random which the encoders can use to generate random numbers however they wish. It's just declared here to
+     * reduce the number of useless variable declarations.
+     */
+    Random random = new Random();
+
     /**
      * Allows to scramble a given string however you want. Should be decode-able using {@link Encoder#decode}
      *
      * @param input The string we want to scramble
      * @return the scrambled key
      */
-    public String encode(String input);
+    String encode(String input);
 
     /**
      * Allows to de-scramble a string scrambled with {@link Encoder#encode} method. Ideally, calling {@code Encoder.decode(Encoder.encode(inputString))}
@@ -19,5 +28,5 @@ public interface Encoder {
      * @param encodedInput The scrambled message we want to unscramble
      * @return The inputted string
      */
-    public String decode(String encodedInput);
+    String decode(String encodedInput);
 }
